4bfc23d6f3ff6f49dd3821c13156d60a73843c08
Mariusz Mycek <m.mycek83@gmail.com>
2019-05-31 16:22:45 +0200

    Change database adress

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://dbUser:<password>@cluster0-ejib9.mongodb.net/test?retryWrites=true&w=majority', {
   useMongoClient: true
 });
 
